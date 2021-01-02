3ae9799626fbf2c0d3625e6f88ec4a92517c0a04
Christoph Eßer <ce@ehser-office-group.de>
2021-01-02 13:15:54 +0100

    try to download on FF iOS

--- my-cookbook.js
+++ my-cookbook.js
@@ -188,22 +188,13 @@ var app = new Vue({
 	    	var downloadLink = document.createElement("a");
 	    	downloadLink.download = fileNameToSaveAs;
 	    	downloadLink.innerHTML = "Download File";
-	    	if (window.webkitURL != null)
-	    	{
-	    		// Chrome allows the link to be clicked
-	    		// without actually adding it to the DOM.
-	    		downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
-	    	}
-	    	else
-	    	{
-	    		// Firefox requires the link to be added to the DOM
-	    		// before it can be clicked.
-	    		downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
-	    		downloadLink.onclick = destroyClickedElement;
-	    		downloadLink.style.display = "none";
-	    		document.body.appendChild(downloadLink);
-	    	}
-	    
+			window.URL = window.URL || window.webkitURL;
+    		
+    		downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    		downloadLink.onclick = destroyClickedElement;
+    		downloadLink.style.display = "none";
+    		document.body.appendChild(downloadLink);
+    
 	    	downloadLink.click();
 	    },
 	    loadFromFile: function (ev) {
