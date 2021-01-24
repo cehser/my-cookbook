3c8d2b0ded691eaef173daf605a6897aa0bc14f8
Christoph Eßer <ce@ehser-office-group.de>
2021-01-24 11:51:28 +0100

    add routing logic for legacy routes

--- src/router/index.js
+++ src/router/index.js
@@ -25,7 +25,10 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Edit.vue')
-  }
+  },
+  //legacy paths for compatibility reasons
+  { path: '/edit.html', redirect: '/edit' }, 
+  { path: '/recipe.html', redirect: '/' }
 ]
 
 const router = new VueRouter({
