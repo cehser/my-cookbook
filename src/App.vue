76f4ea46d47df7604a4c98f46633d02c29f05eb7
Christoph Eßer <ce@ehser-office-group.de>
2021-02-21 20:51:47 +0100

    Bump version to 0.3 save autosync results to local idb

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,19 @@
       this.$store.dispatch("loadRecipes");
       this.$store.dispatch("loadRecipePictures");
 
+      //time in ms
+      const interval = 60000
+
       setInterval(() => {
         if(this.$store.state.settings.autosync) {
           console.log('syncing with cloud')
           this.$store.dispatch('syncRecipesWithCloud')
+            .then(() => {
+              this.$store.dispatch('saveRecipes')
+              this.$store.dispatch('saveRecipePictures')
+            })
         }
-      }, 60000)
+      }, interval)
     }
   }
 </script>
